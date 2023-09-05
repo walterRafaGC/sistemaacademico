494591034c37570554abda2e490a036571094223
walter <wrafagc@gmail.com>
2023-09-05 04:12:31 -0500

    primer prueba

---
+++ src/main/java/com/example/sistemaacademico/SistemaacademicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemaacademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaacademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaacademicoApplication.class, args);
+	}
+
+}
