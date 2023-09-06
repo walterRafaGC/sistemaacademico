a28cecacd4d3073adcd8b946932035feb64da10e
PietroGm <u17211760@utp.edu.pe>
2023-09-06 00:59:52 -0500

    cambio espacio modelo profesor java

---
+++ Back/src/main/java/model/profesor.java
@@ -0,0 +1,46 @@
+public class profesor {
+
+private String nombre;
+private int edad;
+private int dni;
+private String codigo;
+ 
+public profesor(){
+    
+}
+public profesor(String nombre, int edad, int dni, String codigo) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.dni = dni;
+    this.codigo = codigo;
+}
+public String getNombre() {
+    return nombre;
+}
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+public int getEdad() {
+    return edad;
+}
+public void setEdad(int edad) {
+    this.edad = edad;
+}
+public int getDni() {
+    return dni;
+}
+public void setDni(int dni) {
+    this.dni = dni;
+}
+public String getCodigo() {
+    return codigo;
+}
+public void setCodigo(String codigo) {
+    this.codigo = codigo;
+}
+
+
+
+}
+
+
