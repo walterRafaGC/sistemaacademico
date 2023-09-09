10c29d53de58c2fd6026a21e34a66b626326ee82
PietroGm <u17211760@utp.edu.pe>
2023-09-08 22:14:34 -0500

    conexion back bd

---
+++ Back/src/main/java/conexion/mysql.java
@@ -0,0 +1,8 @@
+package conexion;
+
+public class mysql{
+    String  db_url = "mysql://usbdwuhbxcy3cjxt:EvhPH1RIMjnxdiG0S8vP@bjtuc0nkttom2clvwxqt-mysql.services.clever-cloud.com:3306/bjtuc0nkttom2clvwxqt";
+    String  user =  "usbdwuhbxcy3cjxt";
+    String  password = "usbdwuhbxcy3cjxt";
+    String  query = "SELECT * FROM"; 
+}
