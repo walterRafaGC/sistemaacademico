0601ee4e618f4188958df84c17c1a1de81c85e4c
PietroGm <u17211760@utp.edu.pe>
2023-09-09 00:13:26 -0500

    conexion bd1

--- Back/src/main/java/conexion/mysql.java
+++ Back/src/main/java/conexion/mysql.java
@@ -1,8 +1,33 @@
 package conexion;
 
-public class mysql{
-    String  db_url = "mysql://usbdwuhbxcy3cjxt:EvhPH1RIMjnxdiG0S8vP@bjtuc0nkttom2clvwxqt-mysql.services.clever-cloud.com:3306/bjtuc0nkttom2clvwxqt";
-    String  user =  "usbdwuhbxcy3cjxt";
-    String  password = "usbdwuhbxcy3cjxt";
-    String  query = "SELECT * FROM"; 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.springframework.boot.autoconfigure.cassandra.Connection;
+
+public class mysql {
+
+    public static void main(String[] args) {
+        
+
+    }
+
+    public static void getConnection() throws SQLException{
+        Connection c;
+        String database = "bjtuc0nkttom2clvwxqt";
+        String user = "usbdwuhbxcy3cjxt";
+        String password = "usbdwuhbxcy3cjxt";
+        String query = "SELECT * FROM";
+        String host = "bjtuc0nkttom2clvwxqt-mysql.services.clever-cloud.com";
+        int port = 3306;
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch(Exception e){
+            System.out.println("Libreria no encontrada: "+ e.getMessage());
+        }
+        String url = String.format("jdbc:mysql://%s", host);
+        c = DriverManager.getConnection("jdbc:mysql://", user, password);
+    }
+
 }
