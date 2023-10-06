3d4a04461dd0784195e0821a8495d6aeded851b4
PietroGm <u17211760@utp.edu.pe>
2023-10-05 20:46:31 -0500

    cambios-registro-apoderado-2

--- Front/src/views/RegistroApoderado.js
+++ Front/src/views/RegistroApoderado.js
@@ -15,11 +15,15 @@ export default function RegistroApoderado() {
             <input type="text" class="form-control" name="nombreApoderado" />
           </div>
           <div class="mb-3">
-            <label for="apellidoApoderado" class="form-label">Apellido Completo del Apoderado:</label>
+            <label for="apellidoApoderado" class="form-label">
+              Apellido Completo del Apoderado:
+            </label>
             <input type="text" class="form-control" name="apellidoApoderado" />
           </div>
           <div class="mb-3">
-            <label for="tipoDocumentoApoderado" class="form-label">Tipo de documento</label>
+            <label for="tipoDocumentoApoderado" class="form-label">
+              Tipo de documento
+            </label>
             <select class="form-select" name="tipoDocumentoApoderado">
               <option selected>Seleccione una opción</option>
               <option value="1">DNI</option>
@@ -27,21 +31,85 @@ export default function RegistroApoderado() {
             </select>
           </div>
           <div class="mb-3">
-            <label for="codigoDocumentoApoderado" class="form-label">Codigo de documento:</label>
-            <input type="number" class="form-control" name="codigoDocumentoApoderado" />
+            <label for="codigoDocumentoApoderado" class="form-label">
+              Codigo de documento:
+            </label>
+            <input
+              type="number"
+              class="form-control"
+              name="codigoDocumentoApoderado"
+            />
           </div>
         </div>
         <hr></hr>
         <h6>Informacion del alumno registrado en la institucion</h6>
         <div>
           <div class="mb-3">
-            <label for="gradoAcademico" class="form-label">Grado Academico Anterior:</label>
+            <label for="colegioProcedencia" class="form-label">
+              Colegio de Procedencia
+            </label>
+            <input type="text" class="form-control" name="colegioProcedencia" />
+          </div>
+          <div class="mb-3">
+            <label for="gradoAcademico" class="form-label">
+              Grado Academico Anterior:
+            </label>
             <input type="text" class="form-control" name="gradoAcademico" />
           </div>
         </div>
         <hr></hr>
-        <h6>Informacion medica del alumno</h6>
+        <h6>Informacion del alumno</h6>
         <div>
+          <div class="mb-3">
+            <label for="genero" class="form-label">
+              Genero:
+            </label>
+            <select class="form-select" name="genero">
+              <option selected>Seleccione una opción</option>
+              <option value="1">Masculino</option>
+              <option value="3">Femenino</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="nacionalidad" class="form-label">
+              Nacionalidad
+            </label>
+            <input type="text" class="form-control" name="nacionalidad" />
+          </div>
+          <div class="mb-3">
+            <label for="edad" class="form-label">
+              Edad:
+            </label>
+            <input type="number" class="form-control" name="edad" />
+          </div>
+          <div class="mb-3">
+            <label for="fecha_nacimiento" class="form-label">
+              Fecha de Nacimiento:
+            </label>
+            <input type="date" class="form-control" name="fecha_nacimiento" />
+          </div>
+          <div class="mb-3">
+            <label for="direccion" class="form-label">
+              Direccion de Domicilio:
+            </label>
+            <input type="text" class="form-control" name="direccion" />
+          </div>
+          <div class="mb-3">
+            <label for="religion" class="form-label">
+              Religion:
+            </label>
+            <input type="text" class="form-control" name="religion" />
+          </div>
+          <div class="mb-3">
+            <label for="habilidadesCompetitivas" class="form-label">
+              Habilidades Competitivas
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              name="habilidadesCompetitivas"
+            />
+          </div>
           <div class="mb-3">
             <label for="sangre" class="form-label">
               Tipo de Sangre:
@@ -52,19 +120,19 @@ export default function RegistroApoderado() {
             <label for="alergias" class="form-label">
               Alergias:
             </label>
-            <textarea
-              class="form-control"
-              rows="2"
-            ></textarea>
+            <textarea class="form-control" rows="2"></textarea>
           </div>
           <div class="mb-3">
             <label for="enfermedadesHereditarias" class="form-label">
               Antecedentes de Enfermedades Hereditarias
             </label>
-            <textarea
-              class="form-control"
-              rows="2"
-            ></textarea>
+            <textarea class="form-control" rows="2"></textarea>
+          </div>
+          <div class="mb-3">
+            <label for="enfermedadesCronicas" class="form-label">
+              Historial de Enfermedades Cronicas
+            </label>
+            <textarea class="form-control" rows="2"></textarea>
           </div>
           <button type="submit" class="btn btn-primary">
             Guardar la Informacion
